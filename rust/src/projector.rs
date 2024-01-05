ae63103433ea50dd106f44633747be9f8bab1fd4
= <=>
2024-01-05 12:10:07 -0600

    fixed clone

--- rust/src/projector.rs
+++ rust/src/projector.rs
@@ -65,9 +65,8 @@ impl Projector {
     pub fn remove_value(&mut self, key: &str) {
         self.data
             .projector
-            .entry(self.config.pwd.clone())
-            .or_default()
-            .remove(key);
+            .get_mut(&self.config.pwd)
+            .map(|x| x.remove(key));
     }
 
     pub fn from_config(config: Config) -> Self {
